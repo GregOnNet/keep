4a592709329ef4f63d57ef7ee33e4f89cfaa2b5b
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-23 15:08:39 +0100

    notes: load notes via effect

--- src/app/notes/store/effects/notes.effects.ts
+++ src/app/notes/store/effects/notes.effects.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { NotesActionTypes } from '../actions/notes.actions';
+import { exhaustMap, map } from 'rxjs/operators';
+import { NotesService } from '../../lib/notes.service';
+import { LoadNotesSuccess, NotesActionTypes } from '../actions/notes.actions';
 
 @Injectable()
 export class NotesEffects {
-
   @Effect()
-  loadNotess$ = this.actions$.pipe(ofType(NotesActionTypes.LoadNotes));
+  loadNotes$ = this.actions$.pipe(
+    ofType(NotesActionTypes.LoadNotes),
+    exhaustMap(() => this._notes.all()),
+    map(notes => new LoadNotesSuccess(notes))
+  );
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private _notes: NotesService) {}
 }
