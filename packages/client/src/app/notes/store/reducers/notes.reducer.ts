9f2d13c3240e3fb8b88db07333bc3da42cf4d164
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-30 15:03:49 +0100

    refactor(notes): extract case reducer

--- packages/client/src/app/notes/store/reducers/notes.reducer.ts
+++ packages/client/src/app/notes/store/reducers/notes.reducer.ts
@@ -12,26 +12,38 @@ const defaults: NotesSlice = {
 export function reducer(slice = defaults, action: NotesActions): NotesSlice {
   switch (action.type) {
     case NotesActionTypes.LoadNotesSuccess:
-      return {
-        ...slice,
-        entities: action.payload
-      };
+      return setNotes(action.payload, slice);
 
     case NotesActionTypes.CreateNoteSuccess:
-      return {
-        ...slice,
-        entities: [...slice.entities, action.payload]
-      };
+      return createNote(action.payload, slice);
 
     case NotesActionTypes.UpdateNoteSuccess:
-      return {
-        ...slice,
-        entities: slice.entities.map(note =>
-          note.guid === action.payload.guid ? action.payload : note
-        )
-      };
+      return updateNote(action.payload, slice);
 
     default:
       return slice;
   }
+}
+
+function setNotes(notes: Note[], slice: NotesSlice): NotesSlice {
+  return {
+    ...slice,
+    entities: notes
+  };
+}
+
+function createNote(note: Note, slice: NotesSlice): NotesSlice {
+  return {
+    ...slice,
+    entities: [...slice.entities, note]
+  };
+}
+
+function updateNote(updates: Note, slice: NotesSlice): NotesSlice {
+  return {
+    ...slice,
+    entities: slice.entities.map(note =>
+      note.guid === updates.guid ? updates : note
+    )
+  };
 }
